c3f8272f19065b4daa6fc1a3f3c8cb9b041af0ff
Shashikant Panchal <shashikant.panchal@excelindia.com>
2025-02-05 20:34:13 +0530

    update

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 // MongoDB connection
 mongoose
   .connect(process.env.MONGODB_URI, {
